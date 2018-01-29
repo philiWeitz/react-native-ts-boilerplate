9e2c6715cc43ba0715594904685eb2789d7287c2
Philipp Weitz <philipp.weitz@futurice.com>
2018-01-29 15:49:47 +0200

    fixed typescript debugging

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.tsx';
+import Index from '../index.android.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
